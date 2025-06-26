0f073c694de96444634deaa0bb477892085d4339
Nishagi <jeewanthadeherath04@gmail.com>
2025-06-26 18:13:17 +0530

    ATTO-16 Add routing for UserSignUp and UserLogin components

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Timeline from './pages/Lawyer/Timeline';
 import Incomes from './pages/Lawyer/Incomes';
 import DaySummary from './pages/Lawyer/DaySummary';
 import DuePayments from './pages/Lawyer/DuePayments';
+import UserSignUp from './pages/Common/UserSignUp';
+import UserLogin from './pages/Common/UserLogin';
 
 
 export default function App() {
@@ -20,6 +22,8 @@ export default function App() {
           <Route path="lawyer/incomes" element={<Incomes />} />
           <Route path="lawyer/day-summary" element={<DaySummary />} />
           <Route path="lawyer/due-payments" element={<DuePayments />} />
+          <Route path="user/signup" element={<UserSignUp />} />
+          <Route path="user/login" element={<UserLogin />} />
         </Routes>
       </div>
     </Router>
