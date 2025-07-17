d073190b1bb28480d36862a68e737098863ee18d
Ishan maduranga <ishanmadurangai662@gmail.com>
2025-07-17 16:56:56 +0530

    [feature/ATTO-48] added the import lines of the code file

---
+++ src/pages/Lawyer/AddDocuments.jsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import Button1 from '../../components/UI/Button1';
+import Button2 from '../../components/UI/Button2';
+import Input1 from '../../components/UI/Input1';
